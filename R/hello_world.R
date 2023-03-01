a642925d368570f7c7cd1136100ba530327caafc
Lauren Brown <laurenbrown@Laurens-MacBook-Air-2.local>
2023-03-01 14:18:21 +0200

    Added initial files to repository

---
+++ R/hello_world.R
@@ -0,0 +1,10 @@
+#' Printing "hello world"
+#'
+#' This function just prints "hello world", nothing else
+#' @return The function returns: "hello world"
+#' @examples 
+#' helloworld()
+#' @export
+helloworld <- function(){
+	print("hello world")
+}
