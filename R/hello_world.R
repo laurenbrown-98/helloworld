3b9074303d7c822061bd64811affbcdc8e6bf3fe
Lauren Brown <laurenbrown@Laurens-MacBook-Air-2.local>
2023-03-07 14:09:19 +0200

    Added some random spaces

--- R/hello_world.R
+++ R/hello_world.R
@@ -3,8 +3,9 @@
 #' This function just prints "hello world", nothing else
 #' @return The function returns: "hello world"
 #' @examples 
-#' helloworld()    
+#' helloworld()
 #' @export
 helloworld <- function(){
-	print("hello world")
+
+	print("hello world")    
 }
