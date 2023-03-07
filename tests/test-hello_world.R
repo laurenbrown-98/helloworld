2e3789b6cfccac9d4d72bf8d4aa57779b8b245eb
Lauren Brown <54535544+laurenbrown-98@users.noreply.github.com>
2023-03-07 15:58:01 +0200

    Update test-hello_world.R
    Added correct "source" to code

--- tests/test-hello_world.R
+++ tests/test-hello_world.R
@@ -1,6 +1,6 @@
 library(testthat)
 context("Testing hello world")
-source("hello_world.R")
+source(here::here("R/hello_world.R"))
 
 test_that(desc = "This prints hello world", code = {
 	test_print <- helloworld()
